c817b17ae9db020f61117a8a06e45187325c4a9e
Mihai Neamt <mihai.neamt@7code.ro>
2022-06-20 11:19:02 +0300

    Add prisma with local db register / login back

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,25 +1,45 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { User } from '@prisma/client';
 
+import { AuthResponseDTO, UserDTO } from './auth.dto';
 import { AuthService } from './auth.service';
-import { UserDTO } from './auth.dto';
 
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('register')
-  @ApiOperation({ description: 'Register user' })
+  @Post('register-cognito')
+  @ApiOperation({ description: 'Register user with Cognito AWS Service' })
   @ApiBody({ type: UserDTO })
-  async register(@Body() user: UserDTO) {
+  async cognitoRegister(@Body() user: UserDTO) {
+    return this.authService.cognitoRegister(user);
+  }
+
+  @Post('login-cognito')
+  @ApiOperation({ description: 'Login user with Cognito AWS Service' })
+  @ApiBody({ type: UserDTO })
+  async cognitoLogin(@Body() user: UserDTO) {
+    return this.authService.cognitoLogin(user);
+  }
+
+  @Post('register')
+  @ApiOperation({
+    description: 'Register user with Prisma Service and proprietary database',
+  })
+  @ApiBody({ type: UserDTO })
+  async register(@Body() user: UserDTO): Promise<User> {
     return this.authService.register(user);
   }
 
   @Post('login')
-  @ApiOperation({ description: 'Login user' })
+  @ApiOperation({
+    description: 'Login user with Prisma Service and proprietary database',
+  })
   @ApiBody({ type: UserDTO })
-  async login(@Body() user: UserDTO) {
+  @ApiResponse({ type: AuthResponseDTO })
+  async login(@Body() user: UserDTO): Promise<AuthResponseDTO> {
     return this.authService.login(user);
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
 import {
   AuthenticationDetails,
   CognitoUser,
@@ -6,22 +8,32 @@ import {
   CognitoUserPool,
   CognitoUserSession,
 } from 'amazon-cognito-identity-js';
+import { GLOBAL_CONFIG } from 'src/configs/global.config';
+import { AuthHelpers } from 'src/shared/helpers/auth.helpers';
 
-import { UserDTO } from './auth.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { UserService } from '../user/user.service';
+
 import { AuthConfig } from './auth.config';
+import { AuthResponseDTO, UserDTO } from './auth.dto';
 
 @Injectable()
 export class AuthService {
   private userPool: CognitoUserPool;
 
-  constructor(private authConfig: AuthConfig) {
+  constructor(
+    private authConfig: AuthConfig,
+    private userService: UserService,
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {
     this.userPool = new CognitoUserPool({
       UserPoolId: this.authConfig.userPoolId,
       ClientId: this.authConfig.clientId,
     });
   }
 
-  async register(user: UserDTO): Promise<CognitoUser> {
+  async cognitoRegister(user: UserDTO): Promise<CognitoUser> {
     const { name, email, password } = user;
 
     return new Promise((resolve, reject) => {
@@ -41,7 +53,7 @@ export class AuthService {
     });
   }
 
-  async login(user: UserDTO): Promise<CognitoUserSession> {
+  async cognitoLogin(user: UserDTO): Promise<CognitoUserSession> {
     const { name, email, password } = user;
 
     const authenticationDetails = new AuthenticationDetails({
@@ -66,5 +78,41 @@ export class AuthService {
         },
       });
     });
+  }
+
+  public async register(user: UserDTO): Promise<User> {
+    return this.userService.createUser(user);
+  }
+
+  async login(user: UserDTO): Promise<AuthResponseDTO> {
+    const userData = await this.userService.findUser({
+      email: user.email,
+    });
+
+    if (!userData) {
+      throw new UnauthorizedException();
+    }
+
+    const isMatch = await AuthHelpers.verify(user.password, userData.password);
+
+    if (!isMatch) {
+      throw new UnauthorizedException();
+    }
+
+    const payload = {
+      id: userData.id,
+      name: userData.name,
+      email: userData.email,
+      password: null,
+    };
+
+    const accessToken = this.jwtService.sign(payload, {
+      expiresIn: GLOBAL_CONFIG.security.expiresIn,
+    });
+
+    return {
+      user: payload,
+      accessToken: accessToken,
+    };
   }
 }
