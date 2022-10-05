25a85f608c0afc9ab280d330c576c039f4c421b0
GarryOne <igor.mardari96@gmail.com>
2022-10-06 00:55:50 +0300

    Added bearerAuth to Swagger

--- src/main.ts
+++ src/main.ts
@@ -44,6 +44,7 @@ async function bootstrap() {
       .setTitle(swaggerConfig.title || 'Nestjs')
       .setDescription(swaggerConfig.description || 'The nestjs API description')
       .setVersion(swaggerConfig.version || '1.0')
+      .addBearerAuth()
       .build();
     const document = SwaggerModule.createDocument(app, options);
 
