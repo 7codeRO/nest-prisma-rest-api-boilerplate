918835d66d9c80b897bc272f5bb490669a073536
GarryOne <igor.mardari96@gmail.com>
2022-10-21 18:09:45 +0300

    Added LoggerMiddleware

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { MiddlewareConsumer } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { UserModule } from '../user/user.module';
@@ -7,9 +8,9 @@ import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { GLOBAL_CONFIG } from '../../configs/global.config';
 import { LoggerModule } from '../logger/logger.module';
-
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
+import { LoggerMiddleware } from '../../middlewares/logger.middleware';
 
 @Module({
   imports: [
@@ -24,4 +25,8 @@ import { AppController } from './app.controller';
   providers: [AppService],
   exports: [],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
